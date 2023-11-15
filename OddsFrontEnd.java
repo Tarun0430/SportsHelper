e4f29690dfbec88865a496070e41af88dffd3874
Tarun Palani <tarun.palani@gmail.com>
2023-11-15 10:19:01 -0600

    set up frontend skeleton

---
+++ OddsFrontEnd.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class OddsFrontEnd extends Application implements OddsFrontEndInterface{
+ /**
+     * Constructor
+     * @param backend
+     * @param scanner
+     */
+     public OddsFrontend(OddsBackEnd backend) {
+        this.backend = backend;
+    }   
+
+     /**
+     * Sets up the stage/display and calls on the following methods when needed
+     * @param primaryStage
+     */
+    public void start(Stage primaryStage) {
+
+    }
+
+     /**
+     * Sets the display for if Add prop is selected
+     */
+    public void AddPropSelected(){
+
+    }
+
+    /**
+     * Sets the display and calls necessary backend methods to save prop
+     */
+    public void savePropSelected(){
+
+    }
+
+    /**
+     * Sets the display and calls necessary backend methods to see saved props
+     */
+    public void showSavedPropsSelected(){
+
+    }
+
+    /**
+     * Sets the display and calls necessary backend methods to clear saved props
+     */
+    public void clearSavedPropsSelected(){
+
+    }
+
+    /**
+     * Takes user back to main menu
+     */
+    public void backButton(){
+
+    }
+
+    /**
+     * Closes app (close stage)
+     */
+    public void exitButton(){
+
+    }
+    
+}
