e527d1952cda429335cb87f6c577dea5735dff01
Tarun Palani <tarun.palani@gmail.com>
2023-11-16 19:38:43 -0600

    Removed unnecessary files

--- OddsFrontEnd.java
+++ OddsFrontEnd.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
--- OddsFrontEndInterface.java
+++ OddsFrontEndInterface.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import javafx.application.Application;
 import javafx.stage.Stage;
 
 public interface OddsFrontEndInterface {
